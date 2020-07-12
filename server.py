59e3eaf390bef3b246f5ca026fdd4e8d28f55a2e
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-12 16:33:57 +0200

    start test bus route

--- server.py
+++ server.py
@@ -1,25 +1,34 @@
 import trio
 import json
+import requests
 from trio_websocket import serve_websocket, ConnectionClosed
 
 
+def fetch_coordinates_route(url):
+    response = requests.get(url)
+    response.raise_for_status()
+    coordinates = response.json()['coordinates']
+    return coordinates
+
 async def echo_server(request):
+    bus_info = {
+        "msgType": "Buses",
+        "buses": [
+            {"busId": "c790сс", "lat": 55.7500, "lng": 37.600, "route": "120"},
+            {"busId": "a134aa", "lat": 55.7494, "lng": 37.621, "route": "670к"},
+        ]
+    }
+    coordinates = fetch_coordinates_route('https://dvmn.org/media/filer_public/aa/10/aa10aaf5-047f-49d6-a7a9-78f98b092161/156.json')
     ws = await request.accept()
     while True:
-        try:
-            # message = await ws.get_message()
-            message = {
-                          "msgType": "newBounds",
-                          "data": {
-                            "east_lng": 37.65563964843751,
-                            "north_lat": 55.77367652953477,
-                            "south_lat": 55.72628839374007,
-                            "west_lng": 37.54440307617188,
-                          },
-                        }
-            await ws.send_message(json.dumps(message))
-        except ConnectionClosed:
-            break
+        for coordinate in coordinates:
+            bus_info['buses'][0]['lat'] = coordinate[0]
+            bus_info['buses'][0]['lng'] = coordinate[1]
+            try:
+                await ws.send_message(json.dumps(bus_info))
+                await trio.sleep(1)
+            except ConnectionClosed:
+                break
 
 async def main():
     await serve_websocket(echo_server, '127.0.0.1', 8000, ssl_context=None)
