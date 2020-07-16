8e570ad81d748668f0712542ad38660afde2f050
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-16 21:27:16 +0200

    first variant with send-receive channel

--- fake_bus.py
+++ fake_bus.py
@@ -19,42 +19,51 @@ def generate_bus_id(route_id,bus_index):
     pass
 
 
-async def run_bus(url,bus_id,route):
+async def run_bus(route,bus_id,send_channel):
     bus_info = {
-                "busId": bus_id,
-                "lat": None,
-                "lng": None,
-                "route": "156"
-            }
-    try:
-        async with open_websocket_url(url) as ws:
-            for coordinates in route:
-                lat,lng = coordinates
-                bus_info['lat'] = lat
-                bus_info['lng'] = lng
-                await ws.send_message(json.dumps(bus_info,ensure_ascii=False))
-    except OSError as ose:
-        print('Connection attempt failed: %s' % ose, file=stderr)
+        "busId": bus_id,
+        "lat": None,
+        "lng": None,
+        "route": bus_id,
+    }
+    async with send_channel:
+            try:
+                for coordinates in route:
+                    lat,lng = coordinates
+                    bus_info['lat'] = lat
+                    bus_info['lng'] = lng
+                    print(bus_info)
+                    await send_channel.send(bus_info)
 
+            except OSError as ose:
+                print('Connection attempt failed: %s' % ose, file=stderr)
+
+async def send_update(server_address,receive_channel):
+    async with open_websocket_url(server_address) as ws:
+
+        async with receive_channel:
+            async for receive in receive_channel:
+                print()
+                await ws.send_message(json.dumps(receive))
 
 async def main():
-    url = 'ws://127.0.0.1:8080'
+    server_address = 'ws://127.0.0.1:8080'
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         async with send_channel, receive_channel:
+            for route in load_routes():
+                bus_id = route['name']
+                route = route['coordinates']
 
-            while True:
-                for route in load_routes():
-                    bus_id = route['name']
-                    route = route['coordinates']
+            # random_position_route = random.randint(0,len(route))
+            # print(type(random_position_route))
+            # random_route = route[random_position_route:]
 
-                    random_position_route = random.randint(0,len(route))
-                    print(type(random_position_route))
-                    random_route = route[random_position_route:]
-
-                    nursery.start_soon(run_bus,url,bus_id,route)
-                    nursery.start_soon(run_bus, url, bus_id, random_route)
-                    await trio.sleep(0.1)
+                nursery.start_soon(run_bus,route,bus_id,send_channel.clone())
+                nursery.start_soon(send_update,server_address,receive_channel.clone())
+                await trio.sleep(1)
+            # nursery.start_soon(run_bus, url, bus_id, random_route)
+            # await trio.sleep(0.1)
 
 if __name__=='__main__':
     trio.run(main)
