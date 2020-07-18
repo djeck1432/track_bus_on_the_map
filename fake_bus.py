5e2f9c7c57cae841e362999583c1171c5155868e
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-18 18:44:03 +0200

    add relaunch

--- fake_bus.py
+++ fake_bus.py
@@ -2,8 +2,24 @@ import os
 import json
 import trio
 from sys import stderr
-from trio_websocket import open_websocket_url
+from trio_websocket import open_websocket_url, HandshakeError
 import random
+import logging
+
+
+logger = logging.getLogger('fake_bus')
+TICK = 0.1
+
+def relaunch_on_disconnect(func):
+    async def wrappers(*args, **kwargs):
+        while True:
+            try:
+                await func(*args,**kwargs)
+            except HandshakeError:
+                logger.info("Doesn't Connect")
+                await trio.sleep(5)
+                logger.info('Relaunch')
+    return wrappers
 
 
 def load_routes(directory_path='routes'):
@@ -23,7 +39,7 @@ def create_description_route(bus_info,route,route_id):
     random_route = route[random_position_bus:]
     for bus_index in range(10):
         bus_info['busId'] = generate_bus_id(route_id,bus_index)
-        for coordinates in route:
+        for coordinates in random_route:
             lat, lng = coordinates
             bus_info['lat'] = lat
             bus_info['lng'] = lng
@@ -40,13 +56,11 @@ async def run_bus(route,route_id,send_channel):
         try:
             buses_info = create_description_route(bus_info,route,route_id)
             for bus_info in buses_info:
-            # for coordinates in route:
-            #     lat,lng = coordinates
-            #     bus_info['lat'] = lat
-            #     bus_info['lng'] = lng
                 await send_channel.send(bus_info)
+                await trio.sleep(TICK)
         except OSError as ose:
-            print('Connection attempt failed: %s' % ose, file=stderr)
+            logger.warning('Connection attempt failed: %s' % ose, file=stderr)
+
 
 
 async def send_update(server_address,receive_channel):
@@ -57,19 +71,23 @@ async def send_update(server_address,receive_channel):
                 await ws.send_message(json.dumps(receive))
 
 
+@relaunch_on_disconnect
 async def main():
+    logging.basicConfig(level=logging.DEBUG)
     server_address = 'ws://127.0.0.1:8080'
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         async with send_channel, receive_channel:
-            for route in load_routes():
-                route_id = route['name']
-                route = route['coordinates']
+            while True:
+                for route in load_routes():
+                    route_id = route['name']
+                    route = route['coordinates']
 
-                nursery.start_soon(run_bus, route, route_id, send_channel.clone())
-                nursery.start_soon(send_update, server_address, receive_channel.clone())
-                await trio.sleep(1)
+                    nursery.start_soon(run_bus, route, route_id, send_channel.clone())
+                    nursery.start_soon(send_update, server_address, receive_channel.clone())
+                    await trio.sleep(TICK)
 
 
 if __name__=='__main__':
     trio.run(main)
+
