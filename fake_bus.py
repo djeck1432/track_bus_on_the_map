5d76c70f696e3c5f27858e1964f3285a3ed1bf7c
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-21 18:40:07 +0200

    added option to settings cli

--- fake_bus.py
+++ fake_bus.py
@@ -5,6 +5,7 @@ from sys import stderr
 from trio_websocket import open_websocket_url, HandshakeError
 import random
 import logging
+import argparse
 
 
 logger = logging.getLogger('fake_bus')
@@ -34,10 +35,10 @@ def load_routes(directory_path='routes'):
 def generate_bus_id(route_id,bus_index):
     return f'{route_id}-{bus_index}'
 
-def create_description_route(bus_info,route,route_id):
+def create_description_route(bus_info,route,route_id,buses_per_route):
     random_position_bus = random.randint(0,len(route))
     random_route = route[random_position_bus:]
-    for bus_index in range(10):
+    for bus_index in range(buses_per_route):
         bus_info['busId'] = generate_bus_id(route_id,bus_index)
         for coordinates in random_route:
             lat, lng = coordinates
@@ -45,7 +46,7 @@ def create_description_route(bus_info,route,route_id):
             bus_info['lng'] = lng
             yield bus_info
 
-async def run_bus(route,route_id,send_channel):
+async def run_bus(route,route_id,buses_per_route,send_channel):
     bus_info = {
         "busId": None,
         "lat": None,
@@ -54,7 +55,7 @@ async def run_bus(route,route_id,send_channel):
     }
     async with send_channel:
         try:
-            buses_info = create_description_route(bus_info,route,route_id)
+            buses_info = create_description_route(bus_info,route,route_id,buses_per_route)
             for bus_info in buses_info:
                 await send_channel.send(bus_info)
                 await trio.sleep(TICK)
@@ -73,21 +74,33 @@ async def send_update(server_address,receive_channel):
 
 @relaunch_on_disconnect
 async def main():
+    parser = argparse.ArgumentParser(description='List of options')
+    parser.add_argument('--server', help='adress of server',default='ws://127.0.0.1:8080')
+    parser.add_argument('-r',default=500, type=int,help='routes_number')
+    parser.add_argument('-x',default=20,type=int,help='buses_per_route')
+    parser.add_argument('-w',default=10,type=int,help='websockets_number')
+    parser.add_argument('--verbose','-v',action='count',help='Switch on logging')
+    parser.add_argument('--emulator_id',default='')
+    args = parser.parse_args()
+
     logging.basicConfig(level=logging.DEBUG)
-    server_address = 'ws://127.0.0.1:8080'
+    server_address = args.server
     async with trio.open_nursery() as nursery:
         send_channel, receive_channel = trio.open_memory_channel(0)
         async with send_channel, receive_channel:
             while True:
-                for route in load_routes():
+                for route in load_routes()[0:args.r]:
                     route_id = route['name']
                     route = route['coordinates']
 
-                    nursery.start_soon(run_bus, route, route_id, send_channel.clone())
+                    nursery.start_soon(run_bus, route, route_id, args.x, send_channel.clone())
                     nursery.start_soon(send_update, server_address, receive_channel.clone())
                     await trio.sleep(TICK)
 
 
 if __name__=='__main__':
-    trio.run(main)
+    try:
+        trio.run(main)
+    except KeyboardInterrupt:
+        pass
 
