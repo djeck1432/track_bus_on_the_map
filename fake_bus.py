1c4dbd55c0b164a7a653cfc58dc5ac41d4067ca1
djeck1432 <oleh.kostashchuk@gmail.com>
2020-07-21 18:45:46 +0200

    added contexlib with KeyboardInterrupt

--- fake_bus.py
+++ fake_bus.py
@@ -3,6 +3,7 @@ import json
 import trio
 from sys import stderr
 from trio_websocket import open_websocket_url, HandshakeError
+from contextlib import suppress
 import random
 import logging
 import argparse
@@ -32,8 +33,8 @@ def load_routes(directory_path='routes'):
 
 
 
-def generate_bus_id(route_id,bus_index):
-    return f'{route_id}-{bus_index}'
+def generate_bus_id(route_id,bus_index,emulator_id):
+    return f'{emulator_id}{route_id}-{bus_index}'
 
 def create_description_route(bus_info,route,route_id,buses_per_route):
     random_position_bus = random.randint(0,len(route))
@@ -46,7 +47,7 @@ def create_description_route(bus_info,route,route_id,buses_per_route):
             bus_info['lng'] = lng
             yield bus_info
 
-async def run_bus(route,route_id,buses_per_route,send_channel):
+async def run_bus(route,route_id,buses_per_route,emulator_id,send_channel):
     bus_info = {
         "busId": None,
         "lat": None,
@@ -55,7 +56,7 @@ async def run_bus(route,route_id,buses_per_route,send_channel):
     }
     async with send_channel:
         try:
-            buses_info = create_description_route(bus_info,route,route_id,buses_per_route)
+            buses_info = create_description_route(bus_info,route,route_id,buses_per_route,emulator_id)
             for bus_info in buses_info:
                 await send_channel.send(bus_info)
                 await trio.sleep(TICK)
@@ -77,30 +78,32 @@ async def main():
     parser = argparse.ArgumentParser(description='List of options')
     parser.add_argument('--server', help='adress of server',default='ws://127.0.0.1:8080')
     parser.add_argument('-r',default=500, type=int,help='routes_number')
-    parser.add_argument('-x',default=20,type=int,help='buses_per_route')
-    parser.add_argument('-w',default=10,type=int,help='websockets_number')
+    parser.add_argument('--buses_per_route','-x',default=20,type=int,help='buses_per_route')
+    parser.add_argument('--websockets_number','-w',default=10,type=int,help='websockets_number')
     parser.add_argument('--verbose','-v',action='count',help='Switch on logging')
-    parser.add_argument('--emulator_id',default='')
+    parser.add_argument('--emulator_id',default='',type=str)
     args = parser.parse_args()
 
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=args.verbose)
     server_address = args.server
-    async with trio.open_nursery() as nursery:
-        send_channel, receive_channel = trio.open_memory_channel(0)
-        async with send_channel, receive_channel:
-            while True:
-                for route in load_routes()[0:args.r]:
-                    route_id = route['name']
-                    route = route['coordinates']
 
-                    nursery.start_soon(run_bus, route, route_id, args.x, send_channel.clone())
-                    nursery.start_soon(send_update, server_address, receive_channel.clone())
-                    await trio.sleep(TICK)
+    with suppress('KeyboardInterrupt'):
+        async with trio.open_nursery() as nursery:
+            send_channel, receive_channel = trio.open_memory_channel(0)
+            async with send_channel, receive_channel:
+                while True:
+                    for route in load_routes()[0:args.r]:
+                        route_id = route['name']
+                        route = route['coordinates']
+
+                        nursery.start_soon(
+                                            run_bus, route, route_id,
+                                            args.buses_per_route,args.emulator_id, send_channel.clone()
+                        )
+                        nursery.start_soon(send_update, server_address, receive_channel.clone())
+                        await trio.sleep(TICK)
 
 
 if __name__=='__main__':
-    try:
-        trio.run(main)
-    except KeyboardInterrupt:
-        pass
+    trio.run(main)
 
